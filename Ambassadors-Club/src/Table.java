5f0d7fd32a2b75262085c447c81d250d26c72de9
ManuelDCoder <ekwabena57@gmail.com>
2021-04-20 09:48:21 +0000

    Modified

--- Ambassadors-Club/src/Table.java
+++ Ambassadors-Club/src/Table.java
@@ -11,4 +11,16 @@ public class Table {
     private JLabel enterNameLabel;
     private JButton updateButton;
     private JButton deleteButton;
+
+
+
+
+    public static void main(String[] args) {
+        JFrame table = new JFrame("Registered Members");
+
+        table.setVisible(true);
+        table.setContentPane(new Table().tablePanel);
+        table.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        table.setSize(400, 400);
+    }
 }
